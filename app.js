1027a56adfd9afd9ddac961b5ec6a756338e7dbd
DopeSoft <khaya.malabie@gmail.com>
2020-05-04 21:35:53 -0400

    inital commit
    1. Initial commit of calculator
    
    2. Added Bulma for css
    
    3. hello world html page.

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator = {
+
+    addNumbers: function (input1, input2) {
+        console.log(input1 + input2);
+    },
+
+    subtractNumbers: function (input1, input2) {
+        console.log(input1 - input2);
+    },
+
+    divideNumbers: function (input1, input2) {
+        console.log(input1 / input2);
+    },
+
+    multiplyNumbers: function (input1, input2) {
+        console.log(input1 * input2);
+    },
+
+};
